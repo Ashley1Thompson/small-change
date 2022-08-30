065cb2e8246030a7bf08a8736d5bd5954d576ffb
canslercp <canslercp@gmail.com>
2022-08-30 12:03:51 -0400

    continued schemas folder

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -38,3 +38,4 @@ const resolvers = {
     }
 }
 
+module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,30 +2,31 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
   type User {
-    _id: ID!
-    username: String!
-    email: String!
-    password: String!
-    goodDeed: []
+    _id: ID
+    username: String
+    email: String
+    password: String
+    goodDeeds: [GoodDeed]!
   }
 
   type GoodDeed {
-    _id: ID!
-    goodDeedText: String!
-    goodDeedAuthor: String!
+    _id: ID
+    goodDeedText: String
+    goodDeedAuthor: String
     createdAt: String
   }
 
   type Query {
+    users: [User]
+    goodDeeds(username: String): [GoodDeed]
     me: User
-    deed: GoodDeed
   }
 
   type Mutation {
+    addUser (username: String!, email: String!, password: String!): Auth
     login (email: String!, password: String!): Auth
-    addUser ()username: String!, email: String!, password: String!): Auth 
-    addGoodDeed (goodDeedText: {}): User
-    deleteGoodDeed (goodDeedText: {}): User
+    addGoodDeed (goodDeedText: String!): GoodDeed
+    removeGoodDeed (goodDeedId: ID!): GoodDeed
   }
 `;
 
