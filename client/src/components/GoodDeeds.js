38acd966b19a381bb4a78adb2ea0af924009a311
canslercp <canslercp@gmail.com>
2022-09-01 20:08:02 -0400

    debugged inspo page

--- client/src/components/GoodDeeds.js
+++ client/src/components/GoodDeeds.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
 
 const GoodDeeds = ({
  goodDeeds,
- title,
- showTitle = true,
  showUsername = true,
 }) => {
 
@@ -16,15 +14,17 @@ const GoodDeeds = ({
           <div key={goodDeed._id} className="card mb-3">
             <h4 className="card-header bg-primary text-light p-2 m-0">
               {showUsername ? (
-                 <Link
-                 className="text-light"
-                 to={`/userProfile`}
-               >
+                <div>
+                  {/* <Link 
+                  className="text-light"
+                  to={`/userProfile/${goodDeed.goodDeedAuthor}`}
+                > */}
                  {goodDeed.goodDeedAuthor} <br />
                  <span style={{ fontSize: '1rem' }}>
                    had this thought on {goodDeed.createdAt}
                  </span>
-               </Link>
+                 </div>
+              //  </Link>
               ) : (
                 <>
                   <span style={{ fontSize: '1rem' }}>
