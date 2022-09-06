bb5fc2acef98938910ba8c8e9f66999c8ea25b68
Corbin Cansler <107365716+canslercp@users.noreply.github.com>
2022-09-06 15:36:12 -0400

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/smallChanges', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/small-change', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
